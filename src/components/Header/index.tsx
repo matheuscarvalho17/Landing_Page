4581ac39fa4915083f9f72cac10c848d0ce76641
matheuscarvalho <matheusinholopescarvalho@gmail.com>
2022-09-22 15:52:15 -0300

    feat : create Home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ import {
   ContentTitle,
 } from './styles';
 
-export default function Header() {
+export function Header() {
   const { asPath } = useRouter();
   const [sidebar, setSidebar] = useState<boolean>(true);
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
-const Home = () => {
-  return (
-    <>
-      <div>This is My Future Landing Page</div>
-    </>
-  );
-};
+import Link from 'next/link';
+import { FaArrowRight } from 'react-icons/fa';
+import {
+  Img,
+  Name,
+  Logo,
+  Intro,
+  Infos,
+  Content,
+  Container,
+  LinkProjects,
+  Especialization,
+} from './styles';
 
-export default Home;
+export default function Home() {
+  return (
+    <Container>
+      <Content>
+        <Infos>
+          <Name>{'Olá, sou Matheus Carvalho'}</Name>
+          <Especialization>{'FrontEnd & Mobile Developer'}</Especialization>
+          <Intro>
+            {`Graduated in Computer Engineering at the Presidente Antônio Carlos University,
+            I currently work as a Front-End developer where I am responsible for developing and
+            maintaining the company's mobile applications, using various technologies in the
+            market such as: Java, Typescript, React-Native and Next.JS.`}
+          </Intro>
+          <Link href="projects">
+            <LinkProjects>
+              {'PROJECTS'}
+              <FaArrowRight />
+            </LinkProjects>
+          </Link>
+        </Infos>
+        <Logo>
+          <Img
+            src="../images/avatar.png"
+            //alt="My avatar"
+          />
+        </Logo>
+      </Content>
+    </Container>
+  );
+}
